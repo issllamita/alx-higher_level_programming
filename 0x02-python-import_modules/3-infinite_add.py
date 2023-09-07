887f2a81edb196a10111f535f4b4d12879151cb0
Mrifag Islam <mrifag.i.slam@gmail.com>
2023-09-07 15:45:34 -0700

    import and modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    s = 0
-    for i in range(1, len(sys.argv)):
-        s += int(sys.argv[i])
-    print(s)
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
